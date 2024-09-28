40c9deae239aa6b04dcdea35b713edf8cc452ada
maurocoimbra <maurocoimbrab@gmail.com>
2024-09-28 13:48:07 -0300

    improving logic

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-$("[type='submit']").click(function() {
-  let text = $(".in").val();
+// automate input
+fetch("text.txt")
+  .then((res) => res.text())
+  .then((text) => {
+    $("#players-raw").val(text)
+   })
+  .catch((e) => console.error(e));
 
-  // if pelada bagres
-  let new_text = text.split("Fabiao: danado")[1];
-  new_text = new_text.split("Lista de espera")[0];
-  new_text = new_text.trim();
-  new_text = new_text.split("\n");
+  $("#convert-list").click(function() {
+    let text = $("#players-raw").val();
+  
+    // if pelada bagres
+    let new_text = text.split("Fabiao: danado")[1];
+    new_text = new_text.split("Lista de espera")[0];
+    new_text = new_text.trim();
+  
+    $("#players-list").val(new_text);
+  })
+
+$("#create-teams").click(function() {
+  let text = $("#players-list").val();
+  new_text = text.split("\n");
 
   // time de 4
   let teams = [];
   for(let i = 0; i < new_text.length; i+=4){
     teams.push(new_text.slice(i, i+4).join("\n"));
   }
-  $(".out").val(teams.join("\n\n"));
+  $("#teams").val(teams.join("\n\n"));
 })
